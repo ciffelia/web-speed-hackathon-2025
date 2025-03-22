b5ef4b0320a98a9cbaabd2dd2f970ac4153da5c2
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-03-22 19:55:19 +0900

    make stream immutable

--- workspaces/server/src/streams.tsx
+++ workspaces/server/src/streams.tsx
@@ -19,6 +19,8 @@ function getTime(d: Date): number {
 
 export function registerStreams(app: FastifyInstance): void {
   app.register(fastifyStatic, {
+    immutable: true,
+    maxAge: '30d',
     prefix: '/streams/',
     root: path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../streams'),
   });
@@ -152,6 +154,10 @@ export function registerStreams(app: FastifyInstance): void {
     return reply.sendFile(
       `${episode.stream.id}.jpg`,
       path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../streams-thumbnail'),
+      {
+        immutable: true,
+        maxAge: '30d',
+      },
     );
   });
 }
