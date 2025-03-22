1ca3db5adece9a28081302cad5553211c99e7e0b
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-03-23 03:49:59 +0900

    fix redos

--- workspaces/client/src/features/auth/logics/isValidEmail.ts
+++ workspaces/client/src/features/auth/logics/isValidEmail.ts
@@ -1,3 +1,20 @@
 export const isValidEmail = (email: string): boolean => {
-  return /^([A-Z0-9_+-]+\.?)*[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i.test(email);
+  const parts = email.split('@');
+  if (parts.length !== 2) {
+    return false;
+  }
+
+  const [localPart, domainPart] = parts as [string, string];
+  // eslint-disable-next-line regexp/prefer-w
+  if (!/^[A-Z0-9_.+-]+$/i.test(localPart)) {
+    return false;
+  }
+  if (localPart.includes('..') || localPart.startsWith('.') || localPart.endsWith('.')) {
+    return false;
+  }
+  if (!/^(?:[A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i.test(domainPart)) {
+    return false;
+  }
+
+  return true;
 };
--- workspaces/client/src/features/auth/logics/isValidPassword.ts
+++ workspaces/client/src/features/auth/logics/isValidPassword.ts
@@ -1,3 +1,4 @@
 export const isValidPassword = (password: string): boolean => {
-  return /^([A-Z0-9_+-]|[A-Z0-9_+-]){3,}$/i.test(password);
+  // eslint-disable-next-line regexp/prefer-w
+  return /^[A-Z0-9_+-]{3,}$/i.test(password);
 };
